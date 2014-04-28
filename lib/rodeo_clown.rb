bd7a638a888996dc4ec4bd7ba634c07a2fb8bcf5
Ted Price <ted.price@gmail.com>
2014-04-28 17:16:21 -0500

    Take env vars 1st, yaml 2nd

--- lib/rodeo_clown.rb
+++ lib/rodeo_clown.rb
@@ -10,11 +10,11 @@ module RodeoClown
   # Just set your aws credentials
   def self.credentials
     @credentials ||= 
-      if File.exists?(file = File.expand_path("~") + "/.rodeo_clown.yml")
-        YAML.load_file(file)
-      elsif ENV.key?("AWS_ACCESS_KEY") && ENV.key?("AWS_SECRET_ACCESS_KEY") 
+      if ENV.key?("AWS_ACCESS_KEY") && ENV.key?("AWS_SECRET_ACCESS_KEY") 
         { access_key_id: ENV["AWS_ACCESS_KEY"],
           secret_access_key_id: ENV["AWS_SECRET_ACCESS_KEY"], }
+      elsif File.exists?(file = File.expand_path("~") + "/.rodeo_clown.yml")
+        YAML.load_file(file)
       else
         raise "Please supply aws_access_key and Aws_secret_access_key"
       end
