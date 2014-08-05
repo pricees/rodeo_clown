29b0d1edac857afd988c41679e57fa4b54cb9586
Benjamin Fleischer <benjamin.fleischer@raise.com>
2014-08-05 16:44:23 -0500

    Update to be run from the raise.config synced directory
    e.g. in here
    1. run rake install
    then in your s3 synced dir for raise.config run
    2. RACK_ENV=test lets_dance deploy/ranch_hands.yml www
    
    this will update your raise.config from s3 before the deploy
    
    this will require no local changes in rodeo_clown
    
    everyone profits

--- lib/rodeo_clown/s3.rb
+++ lib/rodeo_clown/s3.rb
@@ -3,28 +3,25 @@ module RodeoClown
   module S3
 
     def self.get_config_files
+      configs = RodeoClown.get_configs('production')
+      credentials =  {
+        access_key_id: configs["access_key_id"],
+        secret_access_key: configs["secret_access_key"],
+      }
+
       s3 = AWS::S3.new(credentials)
 
       s3.buckets["raise.config"].objects.each do |s3_file|
-        File.open(determine_path_for(s3_file.key), 'w') do |local_file|
+        new_local_file = determine_path_for(s3_file.key)
+        File.open(new_local_file, 'w') do |local_file|
+          puts "Writing #{new_local_file}"
           local_file.write(s3_file.read)
         end
       end
     end
 
-    def self.credentials
-      @credentials ||=
-      if ENV.key?("AWS_KEY") && ENV.key?("AWS_SECRET_KEY") 
-        { access_key_id: ENV["AWS_KEY"],
-          secret_access_key: ENV["AWS_SECRET_KEY"], }
-      else
-        raise "Please supply AWS_KEY and AWS_SECRET_KEY"
-      end
-    end
-
     def self.determine_path_for(file_name)
-      path = file_name == "deploy.rb" ? "/" : "/deploy/"
-      "#{File.expand_path("config")}#{path}#{file_name}"
+      File.join(File.expand_path("."),file_name)
     end
   end
 
